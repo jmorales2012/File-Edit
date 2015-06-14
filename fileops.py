ede581f1c79ff1d2df3feed3d58b2f90bca331cb
jmorales2012 <jmorales8191@gmail.com>
2015-06-13 22:43:06 -0600

    Edited loop, added functions

--- fileops.py
+++ fileops.py
@@ -1,48 +1,66 @@
 # Read, Write, Empty, Copy files
-
 from os.path import os, exists
 
-selections = """\nWhat would you like to do with this file?\n 	        
-Add to the file? (Enter 'A')\nDelete file? (Enter 'D')\nRead what's in the file? (Enter 'R')
-Copy over to new file? (Enter 'C')"""
-filename = ""
-choice  = " "		# set our choice variable to blank
-answers = "adrcn"		# set our answer string to test for input
+def read_file(filename):										# create function for reading a file
+	target = open(filename)										# opens entered file
+	print "\nHere is your file:\n\n", target.read()				# prints file to screen
+	target.close()												# closes file
 
-while choice not in answers: 				# as long as choice isn't found in our answer string, follow code below
-	filename = raw_input("\nWhat file would you like to modify?\n")	# enter filename to modify
+def append_file(filename):										# create function for appending a file
+	target = open(filename, 'a+')								# open file with append rights
+	append = raw_input("\nEnter the text you would like to add:\n")
+	target.write(append + "\n")									# appends entered text to end of file
+	target.close()												# close file to save it
+
+
+def copy_file(filename):										# create function for copying a file
+	target = open(filename, 'r+')								# open the file with read/write rights
+	new_file = raw_input("\nEnter the name of the file you'd like to copy to: ")
+	out_file = open(new_file, 'w')								# open the new file, truncating it
+	indata = target.read()										# copies data from old file to new string
+	out_file.write(indata)										# write into new file with string from old file
+	print "\nYour file has been copied to: ", new_file
+	out_file.close()											# close new file to save it
+	print "\nHere is your new file: \n\n", open(new_file).read()# prints new file to screen
+
+def delete_file(filename):										# create function to delete a file
+	if exists(filename):										# checks to see if file exists
+		os.remove(filename)										# if it does, delete it
+		print "\nYour file has been deleted!"
+	else:														# if not, print that and set choice to blank to repeat loop
+		print "\nThat file does not exist. Please try again.\n"
+		choice = " "
+
+def second_choice(choice):										# create function for allowing user another choice at end of program
+	choice = raw_input("Would you like to do anything else? (Y/N): ").lower()
+	if choice == 'y':											# if choice is 'y' return a blank space to repeat loop
+		return " "
+	else: 														# if not, return 'e' to exit loop
+		return "e"
+
+selections = """\nWhat would you like to do?\n\n1. Read a file? (Enter 'R')
+2. Create/Add to a file? (Enter 'A')\n3. Copy a file? (Enter 'C')\n4. Delete a file? (Enter 'D')
+5. Exit (Enter 'E')\n"""
+choice = " "													# set choice to blank space to initiate loop
+answers = "racde"												# string of answers to check in loop
+
+while choice not in answers:									# initiate loop
 	print selections
-	choice = raw_input("\nEnter your selection: ").lower()		# ask for user input, convert it to lowercase
-	if choice == "a":											# testing for input
-		target = open(filename, "a+")							# open our filename with append rights and set target equal to it
-		append = raw_input("\nOkay, enter what you would like to add:\n")	# ask for user input to append to file
-		target.write(append + "\n")								# write what we appended to file plus a new line to keep it clean
-		target.close()											# close the file in order to save it
-		print "\nThis is your edited file: \n"
-		target = open(filename)									# re-open the file
-		print target.read()										# print our file to the screen
-	elif choice == "d":
-		if exists(filename):
-			os.remove(filename)
-			print "\nYour file has been deleted!"
-		else:
-			print "That file does not exist. Please try again.\n"
-			choice = ""
-	elif choice == "r":			
-		target = open(filename)									# open our file with read rights, automatically 'read' if arg is empty
-		print "Here is your file:\n"
-		print target.read()										# print our file to the screen
-		target.close()											# close to save file
-	elif choice == "c":
-		target = open(filename, "r+")							# open old file with read+ rights so it can read/write w/o truncating
-		newfile = raw_input("\nWhat file would you like to copy to?\n")	# asks for name of file you would like to copy to
-		out_file = open(newfile, 'w')							# opens that file (creates it if it doesn't exist), empties before writing
-		indata = target.read()									# data that we want to transfer over is our target file
-		out_file.write(indata)									# writes over our new file with data from target file
-		print "\nYour file has been copied to: " + newfile
-		out_file.close()										# close the 2 files to save over them
-		target.close()
-		out_file = open(newfile)								# re-opens new file to view contents
-		print "\nHere is your new file: \n\n" + out_file.read()									# print new file to screen
-	else:
-		print "\nYou didn't make a valid selection. I'm sorry, try again."		# repeat loop if invalid input
+	choice = raw_input("\nEnter your selection: ").lower()		# convert selection to lowercase, assign to choice
+	filename = raw_input("\nEnter file name to modify: ")		# enter file name
+	if not exists(filename) and choice != 'a':					# check to see if filename exists and if choice isn't 'a'
+		print "\nSorry, that file doesn't exist.\n"				# checking for 'a' because we can create new file with choice 'a'
+		choice = " "
+	elif choice == 'r':											# if 'r', call read_file
+		read_file(filename)
+	elif choice == 'a':											# if 'a', call append_file
+		append_file(filename)
+	elif choice == 'c':											# if 'c', call copy_file
+		copy_file(filename)
+	elif choice == 'd':											# if 'd', call delete_file
+		delete_file(filename)
+	elif choice == 'e':											# if 'e', exit program
+		break
+	else:														# if choice isn't a valid selection, repeat loop
+		print "\nNot a valid selection. Please try again."
+	choice = second_choice(choice)								# call second_choice to see if user wants to run program again
